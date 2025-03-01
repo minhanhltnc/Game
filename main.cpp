08da55414966b70f23985e307c3d425266a20483
minhanhltnc <nguyenminhanh1905nt@gmail.com>
2025-03-01 21:58:09 +0700

    new chage

--- main.cpp
+++ main.cpp
@@ -1,7 +1,55 @@
 #include <iostream>
-using namespace std;
-int main()
+#include <SDL.h>
+#include <SDL_image.h>
+#include <stdio.h>
+//screen dimension constants.
+const int SCREEN_WIDTH=640;
+const int SCREEN_HEIGT=480;
+
+//Starts up SDL and creates window
+bool init();
+
+//Loads media
+bool loadMedia();
+
+//Frees media and shuts down SDL
+void close();
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+//The image we will load and show on the screen
+SDL_Surface* gHelloWorld = NULL;
+
+bool init()
 {
-    cout<<"hello";
-    return 0;
+    bool success=true;
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	{
+		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+		success = false;
+	}
+	else
+	{
+		//Create window
+		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if( gWindow == NULL )
+		{
+			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+			success = false;
+		}
+		else
+		{
+			//Get window surface
+			gScreenSurface = SDL_GetWindowSurface( gWindow );
+		}
+	}
+
+	return success;
+}
+
+
+
 }
