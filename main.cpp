1d185cab9d067b55e771ad18b9499e2fcd8c1ba3
minhanhltnc <nguyenminhanh1905nt@gmail.com>
2025-04-07 22:27:27 +0700

    mygame

--- main.cpp
+++ main.cpp
@@ -1,55 +1,70 @@
 #include <iostream>
 #include <SDL.h>
 #include <SDL_image.h>
-#include <stdio.h>
-//screen dimension constants.
-const int SCREEN_WIDTH=640;
-const int SCREEN_HEIGT=480;
+#include "graphics.h"
+#include "defs.h"
 
-//Starts up SDL and creates window
-bool init();
+using namespace std;
 
-//Loads media
-bool loadMedia();
-
-//Frees media and shuts down SDL
-void close();
-
-//The window we'll be rendering to
-SDL_Window* gWindow = NULL;
-
-//The surface contained by the window
-SDL_Surface* gScreenSurface = NULL;
-//The image we will load and show on the screen
-SDL_Surface* gHelloWorld = NULL;
-
-bool init()
+void waitUntilKeyPressed()
 {
-    bool success=true;
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-		success = false;
-	}
-	else
-	{
-		//Create window
-		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( gWindow == NULL )
-		{
-			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-			success = false;
-		}
-		else
-		{
-			//Get window surface
-			gScreenSurface = SDL_GetWindowSurface( gWindow );
-		}
-	}
-
-	return success;
+    SDL_Event e;
+    while (true) {
+        if ( SDL_PollEvent(&e) != 0 &&
+             (e.type == SDL_KEYDOWN || e.type == SDL_QUIT) )
+            return;
+        SDL_Delay(100);
+    }
 }
+/*
+int madfgdgin(int argc, char *argv[])
+{
+    SDL_Window* window = initSDL(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
+    SDL_Renderer* renderer = createRenderer(window);
 
+    SDL_Texture* background = loadTexture("bikiniBottom.jpg", renderer);
+    SDL_RenderCopy( renderer, background, NULL, NULL);
 
+    SDL_RenderPresent( renderer );
+    waitUntilKeyPressed();
 
+    SDL_Texture* spongeBob = loadTexture("Spongebob.png", renderer);
+    renderTexture(spongeBob, 200, 200, renderer);
+
+    SDL_RenderPresent( renderer );
+    waitUntilKeyPressed();
+
+    SDL_DestroyTexture( spongeBob );
+    spongeBob = NULL;
+    SDL_DestroyTexture( background );
+    background = NULL;
+
+    quitSDL(window, renderer);
+    return 0;
+}*/
+
+int main(int argc, char *argv[])
+{
+    Graphics graphics;
+    graphics.init();
+
+    SDL_Texture* background = graphics.loadTexture("bikiniBottom.jpg");
+    graphics.prepareScene(background);
+
+    graphics.presentScene();
+    waitUntilKeyPressed();
+
+    SDL_Texture* spongeBob = graphics.loadTexture("Spongebob.png");
+    graphics.renderTexture(spongeBob, 200, 200);
+
+	graphics.presentScene();
+	waitUntilKeyPressed();
+
+	SDL_DestroyTexture( spongeBob );
+    spongeBob = NULL;
+    SDL_DestroyTexture( background );
+    background = NULL;
+
+    graphics.quit();
+    return 0;
 }
